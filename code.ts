d35d37f57cf93961910e23e716fd793d5014644d
Ruslan Fedorov <anarsis007@gmail.com>
2023-02-15 01:45:32 +0300

    added split TextLayer by nbsp and regular space

--- code.ts
+++ code.ts
@@ -53,7 +53,7 @@ async function setNbspAfterWords () {
 
       // Prepare words of TextLayer and final string
       var finalString = '';
-      var splitWords = node.characters.split(' ');
+      var splitWords = node.characters.split(/ | /);
 
       // Handle every word in TextLayer
       for (const rawWord of splitWords) {
